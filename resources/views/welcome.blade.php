af41b95c03002d32da9d83d4538250fd74425f3f
Bryan Yunis <bbyunis@gmail.com>
2019-06-14 12:42:25 -0600

    add auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    bryGram
                 </div>
 
                 <div class="links">
