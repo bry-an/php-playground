eb486024d36d1a0f711f8190739330a85ee0aad7
Bryan Yunis <bbyunis@gmail.com>
2019-06-15 11:39:12 -0600

    allow user to upload images in post create

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct() // this is constructor
+    {
+        $this->middleware('auth');
+        // every instance of controller has auth middleware, every function
+        // (route) below will require authorization
+
+    }
     public function create()
     {
         return view('posts.create'); // can use period or slash to access files w/i a dir
@@ -15,14 +22,19 @@ class PostsController extends Controller
     {
         $data = request()->validate([
             'caption' => 'required',
-            'image' => ['required', 'image']
+            'image' => ['required', 'image'] // 'image' will be instance of UploadedFile
         ]);
 
+        $imagePath = request('image')->store('uploads', 'public'); // first argument is dir second is driver to use (could use s3)
+
         // get authenticated user
-        auth()->user()->posts()->create($data); // posts() is available under user b/c association
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath
+        ]); // posts() is available under user b/c relationship
+        // Laravel will add user id because it knows about the relationship
+        // \App\Post::create($data); can't use this since user isn't authenticated
 
-        \App\Post::create($data);
-
-        dd(request()->all());
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
